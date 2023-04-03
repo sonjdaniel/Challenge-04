55d5a37b47d32bd22ed38b25b81b89e17b5b9768
Daniel Son <sonjdaniel@gmail.com>
2023-04-02 20:30:00 -0500

    adding more question, adding final score  and final page

--- Assets/script.js
+++ Assets/script.js
@@ -20,6 +20,7 @@ var highScoresPage = document.querySelector(".score_result");
 var replay_button = document.querySelector("#replay");
 var finalScore;
 var initials;
+var highScores = JSON.parse(localStorage.getItem("userInput")) || [];
 
 
 
@@ -31,6 +32,28 @@ var questions = [
     answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
     correctAnswer: "3. Alerts",
   },
+  {
+    question: "The condition is an if/else statement is enclosed with _________.",
+    answers: ["1. Quotes", "2. Curly brackets", "3. Parenthesis", "4. Square brackets"],
+    correctAnswer: "3. Parenthesis"
+},
+{
+    question: "Arrays in JavaScript can be used to store _________.",
+    answers: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
+    correctAnswer: "4. All of the above"
+},
+{
+    question: "String values must be enclosed within _______ when being assigned to variables.",
+    answers: ["1. Commas", "2. Curly brackets", "3. Quotes", "4. Parenthesis"],
+    correctAnswer: "3. Quotes"
+},
+{
+
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ["1. JavaScript", "2. Terminal/Bash", "3. For Loops", "4. Console.log"],
+    correctAnswer: "4. Console.log"
+}
+
 ];
 
 // listen to the answer click
@@ -98,3 +121,97 @@ answerbuttons.style.display = "none";
 resultPage.style.display = "block";
 
 score.textContent = "Your final score is " + finalScore + ".";
+
+// Score page
+function scorePage() {
+        finalScore = timeLeft;
+    
+        question.style.display = "none";
+        answerbuttons.style.display = "none";
+        resultPage.style.display = "block";
+    
+        score.textContent = "Your final score is " + finalScore + ".";
+    
+    }
+    //  All save data
+    function saveUserInput() {
+        highScores.push({
+            initials: input.value,
+            score: finalScore
+        })
+    
+        localStorage.setItem("userInput", JSON.stringify(highScores));
+    
+    }
+   
+    // Final page
+    function showFinalPage() {
+    
+    
+        resultPage.style.display = "none";
+        timer.style.display = "none";
+        finalPage.style.display = "block";
+    
+        output.innerHTML = "";
+    
+        for (var i = 0; i < highScores.length; i++) {
+            var highScore = highScores[i];
+            var li = document.createElement("li");
+            li.textContent = highScore.initials + " - " + highScore.score;
+            console.log(highScore);
+            li.setAttribute("data-index", i);
+            output.appendChild(li);
+        }
+    
+        console.log(highScores);
+    }
+    
+    
+    submitButton.addEventListener("click", function (event) {
+    
+        event.preventDefault();
+    
+        if (input.value === "") {
+    
+            alert("Please enter your initials");
+    
+            return;
+    
+        } else {
+            saveUserInput();
+            showFinalPage();
+        }
+    })
+    
+    highScoresPage.addEventListener("click", function (event) {
+    
+        question.style.display = "none";
+        quiz.style.display = "none";
+        startButton.style.display = "none";
+        resultPage.style.display = "none";
+        timer.style.display = "none";
+        finalPage.style.display = "none";
+    
+    
+        return showFinalPage();
+    
+    })
+
+
+// Allow to play the game again
+replay_button.addEventListener("click", rePlay)
+
+function rePlay() {
+    question.style.display = "block";
+    question.innerHTML = "Coding Quiz Challenge";
+    quiz.style.display = "flex";
+    quiz.classList.add("center_text");
+    startButton.style.display = "block";
+    timer.style.display = "flex";
+    countdown.innerHTML = "60";
+    finalPage.style.display = "none";
+    timeLeft = 60;
+    currentQuestion = 0;
+    input.value = "";
+
+}
