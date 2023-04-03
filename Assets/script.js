5e69018e3637e7c37d93c968b807d20a49655bb3
Daniel Son <sonjdaniel@gmail.com>
2023-04-02 20:21:08 -0500

    Adding score, and connect the html elements

--- Assets/script.js
+++ Assets/script.js
@@ -8,86 +8,93 @@ var answerButton2 = document.querySelector("#answer2");
 var answerButton3 = document.querySelector("#answer3");
 var answerButton4 = document.querySelector("#answer4");
 var countdown = document.querySelector(".countdown");
-var instruction = document.querySelector(".instuction")
-var currentQuestion = 0
+var instruction = document.querySelector(".instuction");
+var currentQuestion = 0;
 var timeLeft = 60;
-
+var resultPage = document.querySelector("#result_page");
+var score = document.querySelector(".score");
+var input = document.querySelector("input");
+var output = document.querySelector("#output");
+var finalPage = document.querySelector("#final_page");
+var highScoresPage = document.querySelector(".score_result");
+var replay_button = document.querySelector("#replay");
+var finalScore;
+var initials;
 
 
 
 // Questions to ask?
+var timeLeft = 60;
 var questions = [
-    {
-        question: "Commonly used data types DO Not Include:",
-        answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-        correctAnswer: "3. Alerts"
-    },
-]
+  {
+    question: "Commonly used data types DO Not Include:",
+    answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    correctAnswer: "3. Alerts",
+  },
+];
 
-
-// listen to the answer click 
+// listen to the answer click
 
 startb.addEventListener("click", function () {
-    questionAsk.textContent = questions[currentQuestion].question;
-    answerButton1.innerHTML = questions[currentQuestion].answers[0]
-    answerButton2.innerHTML = questions[currentQuestion].answers[1]
-    answerButton3.innerHTML = questions[currentQuestion].answers[2]
-    answerButton4.innerHTML = questions[currentQuestion].answers[3]
-    instruction.style = "display: none";
-    startb.style = "display: none";
-    answerButtons.style = "display: flex";
-    answerButtons.style = "visibility: visible";
-
- 
+  questionAsk.textContent = questions[currentQuestion].question;
+  answerButton1.innerHTML = questions[currentQuestion].answers[0];
+  answerButton2.innerHTML = questions[currentQuestion].answers[1];
+  answerButton3.innerHTML = questions[currentQuestion].answers[2];
+  answerButton4.innerHTML = questions[currentQuestion].answers[3];
+  instruction.style = "display: none";
+  startb.style = "display: none";
+  answerButtons.style = "display: flex";
+  answerButtons.style = "visibility: visible";
 });
-
 
 // Timer
 function countDown() {
-    count = 60; 
-    let interval = setInterval(counting,1000); 
-    function counting(){
-        count--; 
-        countdown.textContent = count;
-        //Goes to the first page/function after 1 second 
-        //Allows both the countdown to start and the first question 
-        if (countdown.textContent == 59){
-            startQuiz();
-        } 
-        //if time runs out and the count gets to zero, it calls the scorePage function to end the quiz 
-        if(count === 0) {
-            clearInterval(interval);
-        }
+  count = 60;
+  let interval = setInterval(counting, 1000);
+  function counting() {
+    count--;
+    countdown.textContent = count;
+    //Goes to the first page/function after 1 second
+    //Allows both the countdown to start and the first question
+    if (countdown.textContent == 59) {
+      startQuiz();
     }
-};
+    //if time runs out and the count gets to zero, it calls the scorePage function to end the quiz
+    if (count === 0) {
+      clearInterval(interval);
+    }
+  }
+}
 // user answer with if else
 answerButtons.addEventListener("click", function (event) {
+  var userChoice = event.target;
+  console.log("Selection: " + userChoice.innerHTML);
 
-    var userChoice = event.target;
-    console.log("Selection: " + userChoice.innerHTML);
+  var userAnswer = userChoice.innerHTML;
 
-    var userAnswer = userChoice.innerHTML;
+  if (currentQuestion === 4) {
+    endTimer();
+    return scorePage();
+  } else if (userAnswer !== questions[currentQuestion].correctAnswer) {
+    timeLeft -= 10;
+    comment.textContent = "Wrong!";
+    answerButtons.append(comment);
+  } else {
+    comment.textContent = "Correct";
+    answerButtons.append(comment);
+  }
 
-    if (currentQuestion === 4) {
-        endTimer();
-        return scorePage();
+  currentQuestion += 1;
 
-    } else if (userAnswer !== questions[currentQuestion].correctAnswer) {
-        timeLeft -= 10;
-        comment.textContent = "Wrong!";
-        answerButtons.append(comment);
-    } else {
-        comment.textContent = "Correct";
-        answerButtons.append(comment);
-    }
-
-    currentQuestion += 1;
-
-    question.textContent = questions[currentQuestion].question;
-    answerButton1.innerHTML = questions[currentQuestion].answers[0]
-    answerButton2.innerHTML = questions[currentQuestion].answers[1]
-    answerButton3.innerHTML = questions[currentQuestion].answers[2]
-    answerButton4.innerHTML = questions[currentQuestion].answers[3]
-
+  question.textContent = questions[currentQuestion].question;
+  answerButton1.innerHTML = questions[currentQuestion].answers[0];
+  answerButton2.innerHTML = questions[currentQuestion].answers[1];
+  answerButton3.innerHTML = questions[currentQuestion].answers[2];
+  answerButton4.innerHTML = questions[currentQuestion].answers[3];
 });
 
+question.style.display = "none";
+answerbuttons.style.display = "none";
+resultPage.style.display = "block";
+
+score.textContent = "Your final score is " + finalScore + ".";
